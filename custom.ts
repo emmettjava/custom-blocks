e76d63755eb1399b066e7389c815c3263e53d413
emmettjava <115763142+emmettjava@users.noreply.github.com>
2022-10-30 07:49:32 -0700

    Initial files for MakeCode project

---
+++ custom.ts
@@ -0,0 +1,18 @@
+/**
+ * Animation blocks
+ */
+//% weight=80 color=#0fbc11 icon=""
+namespace animations {
+    /**
+     * Blink an LED at a particular interval
+     * @param x horizontal led cordinate
+     * @param y vertical led cordinate
+     * @param interval time in milli seconds between movement
+     */
+    //% block= "Blink x $x y $y every $interval ms"
+    export function blink(x: number, y: number, interval: number): void {
+
+    }
+
+
+}
